f4327e71a4ed8560da077887e88320529e5afa30
mcnak1231 <kianmcnally1@gmail.com>
2019-03-18 16:29:47 +0000

    garbage message

---
+++ game.py
@@ -0,0 +1,1 @@
+print("Darkness came like the thick velvet curtains of the theatre. It was as if the daytime had been one part of a play and the rest was to come after this intermission of night. You")
