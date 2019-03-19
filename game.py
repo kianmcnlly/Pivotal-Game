d97044ba0a2e7d1cc238a9417803137ccea56b22
mcnak1231 <kianmcnally1@gmail.com>
2019-03-19 15:28:28 +0000

    Complete map for floor 1 python text game Pivotal

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import os
 def help_menu():
     print('hi')
 def start_game():
-    print('hi')
+    print('hello')
 def options():
   options_choice = input(">").lower().strip()
   if options_choice  == "play":
@@ -25,392 +25,295 @@ def options_menu():
   print("          ·Quit·          ")
   options()
 options_menu()
-def map():
-    area_map:{
-        floor1 :{
-            a1:{
-            up = '',
-            down = 'a2',
-            left = '',
-            right =  'b1,'
-            examine =
-            description =
-            },
-            a2:{
-            up = 'a1',
-            down = 'a3',
-            left = '' ,
-            right = 'b2',
-            examine =
-            description =
-            },
-            a3:{
-            up = 'a2',
-            down = 'a4',
-            left = '',
-            right = '',
-            examine =
-            description =
-            },
-            a4:{
-            up = 'a3',
-            down = 'a5',
-            left = '',
-            right = '',
-            examine =
-            description =
-            },
-            a5:{
-            up = 'a4',
-            down = '',
-            left = ''
-            right =
-            examine =
-            description =
-            },
-            a7:{
-            up = '',
-            down = '',
-            left = '',
-            right = 'b7',
-            examine =
-            description =
-            },
-            b1:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b4:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b6:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            b7:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c1:{
-            up =
-            down =
-            left = examine =
-            right =
-            examine =
-            description =
-            },
-            c2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c4:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c6:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            c7:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d1:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d4:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            d6:{
-            up =
-            down =
-            left =
-            right =
-            examine
-            description =
-            },
-            d7:{
-            up =
-            down =
-            left =
-            right =
-            examine
-            description =
-            },
-
-            e1:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e4:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e6:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            e7:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f1:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f4:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f6:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            f7:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g1:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g2:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g3:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g4:{
-            up =
-            down =
-            left =
-            right =
-            examine
-            description =
-            },
-            g5:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g6:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-            g7:{
-            up =
-            down =
-            left =
-            right =
-            examine =
-            description =
-            },
-        }
+# def map():
+#     area_map:{
+#         floor1 :{
+#             a1:{
+#             up = '',
+#             down = 'a2',
+#             left = '',
+#             right =  'b1',
+#             examine =
+#             description =
+#             },
+#             a2:{
+#             up = 'a1',
+#             down = 'a3',
+#             left = '' ,
+#             right = 'b2',
+#             examine =
+#             description =
+#             },
+#             a3:{
+#             up = 'a2',
+#             down = 'a4',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             a4:{
+#             up = 'a3',
+#             down = 'a5',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             a5:{
+#             up = 'a4',
+#             down = '',
+#             left = '',
+#             right = 'b5',
+#             examine =
+#             description =
+#             },
+#             a7:{
+#             up = '',
+#             down = '',
+#             left = '',
+#             right = 'b7',
+#             examine =
+#             description =
+#             },
+#             b1:{
+#             up = '',
+#             down = 'b2',
+#             left = 'a1',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             b2:{
+#             up = 'b1',
+#             down = '',
+#             left = 'a2',
+#             right = 'c2',
+#             examine =
+#             description =
+#             },
+#             b5:{
+#             up = '',
+#             down = '',
+#             left = 'a5',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             b7:{
+#             up = '',
+#             down = '',
+#             left = 'a7',
+#             right = 'c7',
+#             examine =
+#             description =
+#             },
+#             c2:{
+#             up = '',
+#             down = 'c3',
+#             left = 'b2',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             c3:{
+#             up = 'c2',
+#             down = '',
+#             left = '',
+#             right = 'd3',
+#             examine =
+#             description =
+#             },
+#             c7:{
+#             up = '',
+#             down = '',
+#             left = 'b7',
+#             right = 'd7',
+#             examine =
+#             description =
+#             },
+#             d3:{
+#             up = '',
+#             down = 'd4',
+#             left = 'c3',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             d4:{
+#             up = 'd3',
+#             down = 'd5',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             d5:{
+#             up = 'd4',
+#             down = 'd6',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             d6:{
+#             up = 'd5',
+#             down = 'd7',
+#             left = '',
+#             right = 'e6',
+#             examine =
+#             description =
+#             },
+#             d7:{
+#             up =  'd6',
+#             down = '',
+#             left = 'c7',
+#             right = 'e7',
+#             examine =
+#             description =
+#             },
+#             e6:{
+#             up = '',
+#             down = 'e7',
+#             left = 'd6',
+#             right = 'f6',
+#             examine =
+#             description =
+#             },
+#             e7:{
+#             up = 'e6',
+#             down = '',
+#             left = 'd7',
+#             right = 'g7',
+#             examine =
+#             description =
+#             },
+#             f1:{
+#             up = 'floor2',
+#             down = 'f2',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             f2:{
+#             up = 'f1',
+#             down = 'f3',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             f3:{
+#             up = 'f2',
+#             down = 'f4',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             f4:{
+#             up = 'f3',
+#             down = 'f5',
+#             left =  '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             f5:{
+#             up = 'f4',
+#             down = 'f6',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             f6:{
+#             up = 'f5',
+#             down = 'f7',
+#             left = 'e6',
+#             right = 'g7',
+#             examine =
+#             description =
+#             },
+#             f7:{
+#             up = 'f6',
+#             down = '',
+#             left = 'e7',
+#             right = 'g7',
+#             examine =
+#             description =
+#             },
+#             g6:{
+#             up = '',
+#             down = 'g7',
+#             left = 'f6',
+#             right = 'h6',
+#             examine =
+#             description =
+#             },
+#             g7:{
+#             up = 'g6',
+#             down = '',
+#             left = 'f7',
+#             right = 'h7',
+#             examine =
+#             description =
+#             },
+#             h1:{
+#             up = '',
+#             down = 'h2',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             h2:{
+#             up = 'h1',
+#             down = 'h2',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             h3:{
+#             up = 'h2,',
+#             down = 'h3',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             h4:{
+#             up = 'h3',
+#             down = 'h5',
+#             left = '',
+#             right = '',
+#             examine
+#             description =
+#             },
+#             h5:{
+#             up = 'h4',
+#             down = 'h6',
+#             left = '',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             h6:{
+#             up = 'h5',
+#             down = 'h7',
+#             left = 'g6',
+#             right = '',
+#             examine =
+#             description =
+#             },
+#             h7:{
+#             up = 'h6',
+#             down = '',
+#             left = 'g7',
+#             right = ','
+#             examine =
+#             description =
+#             }
+#         }
